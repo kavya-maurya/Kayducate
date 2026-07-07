05489183c8657f543ef65aaf216b21e7d84071d9
kavya <kavyamaurya269@gmail.com>
2026-07-07 19:18:17 +0530

    update route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const logger = require("../config/logger");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
