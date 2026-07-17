e2979b7a4dba1eff71c7a4cae61ed3e440ca5927
kavya <kavyamaurya269@gmail.com>
2026-07-17 21:12:54 +0530

    middleware updated

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const taskController = require("../controllers/task.controller");
 
-const authMiddleware = require("../middleware/errorMiddleware");
+const authMiddleware = require("../middleware/verifyToken");
 
 router.post("/", authMiddleware, taskController.createTask);
 
